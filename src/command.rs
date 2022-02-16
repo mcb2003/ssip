7400f2753211a1c4a43a8d203f95f239fe6f52df
Michael Connor Buchan <mikey@blindcomputing.org>
2022-02-16 16:28:41 +0000

    Add Command enum to represent every valid SSIP command
    Current variants are speak, char, sound icon, stop, cancel, pause,
    resume.and

---
+++ src/command.rs
@@ -0,0 +1,94 @@
+use std::fmt;
+
+use super::{ClientId, Speech};
+
+#[derive(Clone, Debug)]
+pub enum Command<'a> {
+    Speak(Speech<'a>),
+    Char(char),
+    SoundIcon(&'a str),
+    Stop(ClientId),
+    Cancel(ClientId),
+    Pause(ClientId),
+    Resume(ClientId),
+}
+
+impl fmt::Display for Command<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Speak(text) => write!(f, "speak\r\n{}\r\n.\r\n", text),
+            Self::Char(c) if *c == ' ' => write!(f, "char space\r\n"),
+            Self::Char(c) => write!(f, "char {}\r\n", c),
+            Self::SoundIcon(s) => write!(f, "sound_icon {}\r\n", s),
+            Self::Stop(id) => write!(f, "stop {}\r\n", id),
+            Self::Cancel(id) => write!(f, "cancel {}\r\n", id),
+            Self::Pause(id) => write!(f, "pause {}\r\n", id),
+            Self::Resume(id) => write!(f, "resume {}\r\n", id),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_speak_display() {
+        let cmd = Command::Speak(Speech::from("Hello, world!"));
+        assert_eq!(cmd.to_string(), "speak\r\nHello, world!\r\n.\r\n");
+    }
+
+    #[test]
+    fn test_char_display() {
+        let cmd = Command::Char('A');
+        assert_eq!(cmd.to_string(), "char A\r\n");
+        let cmd = Command::Char(' ');
+        assert_eq!(cmd.to_string(), "char space\r\n");
+    }
+
+    #[test]
+    fn test_sound_icon_display() {
+        let cmd = Command::SoundIcon("beep");
+        assert_eq!(cmd.to_string(), "sound_icon beep\r\n");
+    }
+
+    #[test]
+    fn test_stop_display() {
+        let cmd = Command::Stop(ClientId::All);
+        assert_eq!(cmd.to_string(), "stop all\r\n");
+        let cmd = Command::Stop(ClientId::Self_);
+        assert_eq!(cmd.to_string(), "stop self\r\n");
+        let cmd = Command::Stop(ClientId::Id(123));
+        assert_eq!(cmd.to_string(), "stop 123\r\n");
+    }
+
+    #[test]
+    fn test_cancel_display() {
+        let cmd = Command::Cancel(ClientId::All);
+        assert_eq!(cmd.to_string(), "cancel all\r\n");
+        let cmd = Command::Cancel(ClientId::Self_);
+        assert_eq!(cmd.to_string(), "cancel self\r\n");
+        let cmd = Command::Cancel(ClientId::Id(123));
+        assert_eq!(cmd.to_string(), "cancel 123\r\n");
+    }
+
+    #[test]
+    fn test_pause_display() {
+        let cmd = Command::Pause(ClientId::All);
+        assert_eq!(cmd.to_string(), "pause all\r\n");
+        let cmd = Command::Pause(ClientId::Self_);
+        assert_eq!(cmd.to_string(), "pause self\r\n");
+        let cmd = Command::Pause(ClientId::Id(123));
+        assert_eq!(cmd.to_string(), "pause 123\r\n");
+    }
+
+    #[test]
+    fn test_resume_display() {
+        let cmd = Command::Resume(ClientId::All);
+        assert_eq!(cmd.to_string(), "resume all\r\n");
+        let cmd = Command::Resume(ClientId::Self_);
+        assert_eq!(cmd.to_string(), "resume self\r\n");
+        let cmd = Command::Resume(ClientId::Id(123));
+        assert_eq!(cmd.to_string(), "resume 123\r\n");
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,23 @@
+mod command;
+pub use command::Command;
 mod speech;
 pub use speech::Speech;
+
+use std::fmt;
+
+#[derive(Clone, Copy, Debug)]
+pub enum ClientId {
+    All,
+    Self_,
+    Id(i32),
+}
+
+impl fmt::Display for ClientId {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::All => write!(f, "all"),
+            Self::Self_ => write!(f, "self"),
+            Self::Id(id) => write!(f, "{}", id),
+        }
+    }
+}
