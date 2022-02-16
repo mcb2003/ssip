037dd48736c558286a96fa0db5af7c3c4e7575ad
Michael Connor Buchan <mikey@blindcomputing.org>
2022-02-16 19:44:15 +0000

    Add CapitalsMode, NotificationParam, Priority, PunctuationLevel enums with Display impls

--- src/lib.rs
+++ src/lib.rs
@@ -21,3 +21,121 @@ impl fmt::Display for ClientId {
         }
     }
 }
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum GetParam {
+    OutputModule,
+    VoiceType,
+    Rate,
+    Pitch,
+    Volume,
+}
+
+impl fmt::Display for GetParam {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::OutputModule => write!(f, "output_module"),
+            Self::VoiceType => write!(f, "voice_type"),
+            Self::Rate => write!(f, "rate"),
+            Self::Pitch => write!(f, "pitch"),
+            Self::Volume => write!(f, "volume"),
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum ListParam {
+    OutputModules,
+    Voices,
+    SynthesisVoices,
+}
+
+impl fmt::Display for ListParam {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::OutputModules => write!(f, "output_modules"),
+            Self::Voices => write!(f, "voices"),
+            Self::SynthesisVoices => write!(f, "synthesis_voices"),
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum NotificationParam {
+    All,
+    Begin,
+    End,
+    Pause,
+    Resume,
+    IndexMark,
+}
+
+impl fmt::Display for NotificationParam {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::All => write!(f, "all"),
+            Self::Begin => write!(f, "begin"),
+            Self::End => write!(f, "end"),
+            Self::Pause => write!(f, "pause"),
+            Self::Resume => write!(f, "resume"),
+            Self::IndexMark => write!(f, "index_mark"),
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub enum Priority {
+    Progress,
+    Notification,
+    Text,
+    Message,
+    Important,
+}
+
+impl fmt::Display for Priority {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Progress => write!(f, "progress"),
+            Self::Notification => write!(f, "notification"),
+            Self::Text => write!(f, "text"),
+            Self::Message => write!(f, "message"),
+            Self::Important => write!(f, "important"),
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub enum PunctuationLevel {
+    None,
+    Some,
+    Most,
+    All,
+}
+
+impl fmt::Display for PunctuationLevel {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::None => write!(f, "none"),
+            Self::Some => write!(f, "some"),
+            Self::Most => write!(f, "most"),
+            Self::All => write!(f, "all"),
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum CapitalsMode {
+    None,
+    Icon,
+    Spell,
+}
+
+impl fmt::Display for CapitalsMode {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::None => write!(f, "none"),
+            Self::Icon => write!(f, "icon"),
+            Self::Spell => write!(f, "spell"),
+        }
+    }
+}
